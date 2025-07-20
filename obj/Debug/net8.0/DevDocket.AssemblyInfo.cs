04eb660cbd1d510ccd6d2ae63f0f92ee9bc467d8
Emon <emon.onnorokom@gmail.com>
2025-07-20 17:33:27 +0600

    Added publish folder with the deployment assets

--- obj/Debug/net8.0/DevDocket.AssemblyInfo.cs
+++ obj/Debug/net8.0/DevDocket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevDocket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5b29424bbc162568e39870a23a21d4a8be76acc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cb524b4d8548c59de3d039cfaf1ea0818675b14")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevDocket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevDocket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
